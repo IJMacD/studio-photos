da8b5674aa6300f37a423c7293fb75f2660729c6
Iain MacDonald <IJMacD@gmail.com>
2017-09-29 17:17:12 +0800

    Update react and webpack

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,46 +1,31 @@
 const path = require('path')
 const webpack = require('webpack')
+const config = require('./webpack.config');
 
-module.exports = {
-  devtool: 'source-map',
+config.devtool = 'source-map',
 
-  entry: [
-    './src/index'
-  ],
+config.entry = [
+  './src/index'
+];
 
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
-    publicPath: '/public/'
-  },
+ config.output = {
+  path: path.join(__dirname, 'public'),
+  filename: 'bundle.js',
+  publicPath: '/public/'
+};
 
-  plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    })
-  ],
+config.plugins = [
+  new webpack.optimize.UglifyJsPlugin({
+    minimize: true,
+    compress: {
+      warnings: false
+    }
+  }),
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  })
+];
 
-  module: {
-    loaders: [
-      { test: /\.js?$/,
-        loader: 'babel',
-        exclude: /node_modules/ },
-      { test: /\.css?$/,
-        loader: 'style!css?modules&camelCase',
-        include: path.join(__dirname, 'src', 'styles') },
-      { test: /\.png$/,
-        loader: 'file' },
-      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader: 'file'}
-    ]
-  }
-}
+module.exports = config;
