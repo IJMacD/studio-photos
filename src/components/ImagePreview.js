62d16837470b2326e65ed9d6cda21675b7635c18
Iain MacDonald <IJMacD@gmail.com>
2017-10-26 15:13:11 +0800

    Extra EXIF information;
    Fix stale data rendering

--- src/components/ImagePreview.js
+++ src/components/ImagePreview.js
@@ -12,28 +12,24 @@ export default class ImagePreview extends React.Component {
             exif: null,
         };
 
-        this.img = new Image();
-        this.img.onload = () => {
-            const self = this;
-            EXIF.getData(this.img, function () {
-                const exif = EXIF.getAllTags(this);
-                console.log(exif);
-                self.setState({ exif });
-            });
-        };
     }
-    componentWillReceiveProps() {
-        this.img.src = this.props.image.full;
+
+    componentWillReceiveProps(nextProps) {
+        if (this.props.image !== nextProps.image) {
+            getExifData(nextProps.image.full).then(exif => this.setState({ exif }));
+        }
     }
+
     componentDidMount() {
-        this.img.src = this.props.image.full;
+        getExifData(this.props.image.full).then(exif => this.setState({ exif }));
     }
+
     render () {
         const image = this.props.image;
         const srcFull = image.full;
         const exif = this.state.exif;
-        const filename = path.basename(decodeURIComponent(srcFull));
-        const foldername = path.basename(path.dirname(decodeURIComponent(srcFull)));
+        const filename = image.name;
+        const foldername = path.dirname(image.key);
 
         return (
             <div className={styles.shade} onClick={this.props.onClose}>
@@ -55,14 +51,20 @@ export default class ImagePreview extends React.Component {
 
 const EXIFData = ({ exif }) => (
     <dl>
+        {exif.DateTime && [<dt>Date</dt>,<dd>{exif.DateTime.replace(/(\d{4}):(\d{2}):(\d{2})/, "$1-$2-$3")}</dd>] }
+        {exif.PixelXDimension && exif.PixelYDimension && [<dt>Resolution</dt>,<dd>{exif.PixelXDimension}x{exif.PixelYDimension}</dd>] }
         {exif.Make && exif.Model && [<dt>Camera</dt>,<dd>{!exif.Model.startsWith(exif.Make) && trim(exif.Make)} {trim(exif.Model)}</dd>] }
         {exif.FNumber && [<dt>F-stop</dt>,<dd>f/{exif.FNumber.toString()}</dd>] }
         {exif.ExposureTime && [<dt>Exposure</dt>,<dd>1/{Math.round(exif.ExposureTime.denominator/exif.ExposureTime.numerator)}s</dd>] }
         {exif.ISOSpeedRatings && [<dt>ISO speed</dt>,<dd>ISO-{exif.ISOSpeedRatings}</dd>] }
-        {exif.DateTime && [<dt>Date</dt>,<dd>{exif.DateTime.replace(/(\d{4}):(\d{2}):(\d{2})/, "$1-$2-$3")}</dd>] }
-        {exif.PixelXDimension && exif.PixelYDimension && [<dt>Resolution</dt>,<dd>{exif.PixelXDimension}x{exif.PixelYDimension}</dd>] }
+        {exif.Flash && [<dt>Flash</dt>,<dd>{exif.Flash}</dd>] }
+        {exif.GPSLatitude && [<dt>Location</dt>,<dd>{formatCoords(exif.GPSLatitude)} {exif.GPSLatitudeRef}<br/>{formatCoords(exif.GPSLongitude)} {exif.GPSLongitudeRef}</dd>] }
     </dl>
 );
+
+function formatCoords(coord) {
+    return `${coord[0]}° ${coord[1]}′ ${coord[2]}″`;
+}
 
 const Icon = ({ name, ...otherProps }) => (
     <i className="material-icons" {...otherProps}>{name.replace(/ -/, "_")}</i>
@@ -70,4 +72,13 @@ const Icon = ({ name, ...otherProps }) => (
 
 function trim (s) {
     return String(s).replace(/\0/g, "").trim();
+}
+
+function getExifData(url) {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => EXIF.getData(img, () => resolve(EXIF.getAllTags(img)));
+        img.onerror = reject;
+        img.src = url;
+    });
 }
